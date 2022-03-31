43cda1dc24c46424b14d457b0d64a64a6343229d
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-03-31 22:21:29 +0100

    Update NavbarMobile index.js

--- src/components/NavbarMobile/index.js
+++ src/components/NavbarMobile/index.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Mobile.css";
+import Dashboard from "./Images/Dashboard.png";
+import Home from "./Images/Home.png";
+import Saved from "./Images/Saved.png";
+import Payment from "./Images/Payment.png";
+import Comment from "./Images/Comment.png";
 
-const Index = () => {
+const NavbarMobile = () => {
   return (
-    <div>
-    <nav>
-    <ul>
-    <li>
-    </li>
-    </ul>
-    </nav>
-    </div>
-  )
-}
+    <div className="nav">
+      <div className="ul">
+        <a href="./">
+          <img src={Home} alt="" />
+          <p>Home</p>
+        </a>
 
-export default Index
+        <a href="./">
+          <img src={Payment} alt="" />
+          <p>Wallet</p>
+        </a>
+
+        <a href="./">
+          <img src={Dashboard} alt="" />
+          <p>Dashboard</p>
+        </a>
+
+        <a href="./">
+          <img src={Saved} alt="" />
+          <p>Requests</p>
+        </a>
+
+        <a href="./">
+          <img src={Comment} alt="" />
+          <p>Messages</p>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarMobile;
