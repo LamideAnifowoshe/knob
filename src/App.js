af35fbacd0e94067a0fad04d1cc34f9dfce77b8a
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-04-02 00:24:53 +0100

    import MySpace and render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar";
 import { useMediaQuery } from "react-responsive";
 import NavbarMobile from "./components/NavbarMobile";
+import MySpace from "./components/MySpace";
 
 function App() {
   const isDesktopOrLaptop = useMediaQuery({
     query: "(min-width: 1224px)",
   });
 
-  return <div>{isDesktopOrLaptop ? <Navbar /> : <NavbarMobile />}</div>;
+  return (
+    <div>
+      {isDesktopOrLaptop ? <Navbar /> : <NavbarMobile />}
+      <MySpace />
+    </div>
+  );
 }
 
 export default App;
