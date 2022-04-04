1a5b99a236e720e7a8be667f91f9804bd4bedc56
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-04-04 10:00:05 +0100

    Update MySpaceDetailView component.

--- src/components/SpaceDetailView/index.js
+++ src/components/SpaceDetailView/index.js
@@ -6,9 +6,13 @@ const SpaceDetailView = () => {
   return (
     <div className="thespace">
       <div className="header">
-        <h2 className="">The Space</h2>
-        <img src={Edit} alt="" />
-        <h5>42, Bajulaiye street, Surulere, Lagos.</h5>
+        <div>
+          <h3 className="the-space">The Space</h3>
+          <h5 className="addy">42, Bajulaiye street, Surulere, Lagos.</h5>
+        </div>
+      </div>
+      <div>
+        <img src={Edit} style={{ width: "36px", height: "36px" }} alt="" />
       </div>
     </div>
   );
