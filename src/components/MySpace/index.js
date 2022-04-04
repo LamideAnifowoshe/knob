a21fb10d4ca831259e52bdd140f5974621264cc1
Olamide Anifowoshe <74327981+LamideAnifowoshe@users.noreply.github.com>
2022-04-04 09:01:16 +0100

    update MySpace

--- src/components/MySpace/index.js
+++ src/components/MySpace/index.js
@@ -7,33 +7,56 @@ const MySpace = () => {
   return (
     <div className="col-2">
       <div className="myspace">
-        <img className="space" src={myspace} alt="" />
+        <div className="my-space-head">
+          <img
+            src={myspace}
+            style={{
+              width: "271px",
+              height: "82px",
+              borderRadius: "8px 8px 4px 4px",
+            }}
+            alt=""
+          />
+          <div className="details">
+            <div>
+              <h5>
+                The Space <strong>.</strong> Lekki, Lagos.
+              </h5>
+            </div>
 
-        <h5>
-          The Space <strong>.</strong> Lekki, Lagos.
-        </h5>
-        <img
-          src={Toggle}
-          alt=""
-          style={{
-            position: "absolute",
-            left: "251px",
-            top: "55px",
-            width: "16px",
-            height: "16px",
-          }}
-        />
-        <ul className="list">
-          <li className="li1">Apartment</li>
-          <li>Estate</li>
-          <li>Furnished</li>
-          <li>Ensuite</li>
-          <li>2 Bed</li>
-          <li>2 Bath</li>
-        </ul>
-        <h4>
-          N50,0000<small>/month for 12 months</small>
-        </h4>
+            <div>
+              <img
+                src={Toggle}
+                alt=""
+                style={{
+                  width: "16px",
+                  height: "16px",
+                }}
+              />
+            </div>
+          </div>
+          <div className="list">
+            <ul>
+              <li className="li1">Apartment</li>
+              <li>Estate</li>
+              <li>Furnished</li>
+              <li>Ensuite</li>
+              <li>2 Bed</li>
+              <li>2 Bath</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="carddetail">
+          <div>
+            <h4>
+              N50,0000<small>/month for 12 months</small>
+            </h4>
+          </div>
+          <div>
+            <h6>Vacant</h6>
+          </div>
+        </div>
       </div>
     </div>
   );
